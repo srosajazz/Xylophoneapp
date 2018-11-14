354d2f5724d46dd55e2f0d2ce27ce934c263c1c0
Sergio Rosa <scarlosjazz@gmail.com>
2018-11-14 13:18:18 -0500

    Make the App Play a Different Sound for Each Button

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,6 +12,9 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     var audioPlayer : AVAudioPlayer!
+    var selectedSoundFileName : String = ""
+    
+    
     let soundArray = ["note1", "note2","note3","note4","note5","note6","note7"]
     
     
@@ -20,14 +23,16 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
-        var selectedSoundFileName : String = soundArray[sender.tag]
+        
+        selectedSoundFileName = soundArray[0]
+        
         print(selectedSoundFileName)
         
         playSound()
     }
     
     func playSound(){
-        let sounfURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
+        let sounfURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
         
         do {
             audioPlayer = try AVAudioPlayer(contentsOf:sounfURL!)
