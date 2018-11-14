55b657d61abf88a3ed7b83be6a365244b5c9d0ef
Sergio Rosa <scarlosjazz@gmail.com>
2018-11-14 12:50:17 -0500

    Create a Method to Contain the Sound Playing Functionality

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -12,11 +12,21 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1", "note2","note3","note4","note5","note6","note7"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
+        var selectedSoundFileName : String = soundArray[sender.tag]
+        print(selectedSoundFileName)
+        
+        playSound()
+    }
+    
+    func playSound(){
         let sounfURL = Bundle.main.url(forResource: "note1", withExtension: "wav")
         
         do {
@@ -27,4 +37,5 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         }
         audioPlayer.play()
     }
+    
 }
